1b325a67278a49fa999b4f3ebed1e96f1b7ef0cf
Nikhil Toram <nikhiltoram@gmail.com>
2024-04-13 13:19:56 -0500

    base map without the shapes

--- simplified main.py
+++ simplified main.py
@@ -1,4 +1,10 @@
 import networkx as nx
-
+import matplotlib.pyplot as plt
+#import pygame
 # Create the triangular lattice graph (3 rows, 5 columns)
 regular_lattice = nx.grid_2d_graph(10, 10)
+pos = {(x, y): (y, -x) for x, y in regular_lattice.nodes()}
+nx.draw(regular_lattice, pos=pos, with_labels=True, node_color='skyblue', node_size=250, font_size=12)
+plt.title("Regular 2d")
+plt.figure(dpi=500)
+plt.show()
